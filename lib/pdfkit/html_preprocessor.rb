c782753e4a8ad6d6b5f8ae1ce4237818e1d18a97
poula <bola.hanna55@gmail.com>
2019-08-10 21:02:11 +0200

    preprocess inline style property background url

--- lib/pdfkit/html_preprocessor.rb
+++ lib/pdfkit/html_preprocessor.rb
@@ -13,11 +13,15 @@ class PDFKit
     def self.translate_relative_paths(html, root_url)
       # Try out this regexp using rubular http://rubular.com/r/hiAxBNX7KE
       html.gsub(/(href|src)=(['"])\/([^\/"']([^\"']*|[^"']*))?['"]/, "\\1=\\2#{root_url}\\3\\2")
+      # Try out this regex using rubular https://rubular.com/r/VOeeYjkuRi1meA
+      html.gsub(/url\(\s*(['"]?)(.*?)\1\s*\)/, "url('#{root_url}\\2')")
     end
 
     def self.translate_relative_protocols(body, protocol)
       # Try out this regexp using rubular http://rubular.com/r/0Ohk0wFYxV
       body.gsub(/(href|src)=(['"])\/\/([^\"']*|[^"']*)['"]/, "\\1=\\2#{protocol}://\\3\\2")
+      # Try out this regex using rubular https://rubular.com/r/VOeeYjkuRi1meA
+      body.gsub(/url\(\s*(['"]?)(.*?)\1\s*\)/, "url('#{protocol}://\\2')")
     end
   end
 end
